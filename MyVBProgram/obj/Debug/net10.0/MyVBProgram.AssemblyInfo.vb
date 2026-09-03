52c4e142aac4e40f6697f8fce0a0171ede4cdeb1
MalachiBell-Prog09 <malachibell270@gmail.com>
2026-09-03 03:49:37 +0000

    invalid input

--- MyVBProgram/obj/Debug/net10.0/MyVBProgram.AssemblyInfo.vb
+++ MyVBProgram/obj/Debug/net10.0/MyVBProgram.AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("MyVBProgram"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c72db0856bb3479d9c20fb3e74bb56ca1d667088"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+497788a301cce75f3ca9d1fe8070f25891de560f"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("MyVBProgram"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("MyVBProgram"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")> 
