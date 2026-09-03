59d99f46bcab61f36b3d35698ee521419626729b
MalachiBell-Prog09 <malachibell270@gmail.com>
2026-09-03 03:33:04 +0000

    age

--- MyVBProgram/obj/Debug/net10.0/MyVBProgram.AssemblyInfo.vb
+++ MyVBProgram/obj/Debug/net10.0/MyVBProgram.AssemblyInfo.vb
@@ -15,7 +15,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("MyVBProgram"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b05b5acbb80ebd1a5e1ed753804d3eda39f0b5c"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2bab7af19701b64457c5ffa76483b65097a5588"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("MyVBProgram"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("MyVBProgram"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")> 
